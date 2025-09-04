059f0e41bcdbc6203cf0e6bd38436a9077753658
Fabricio Iaronka <fabricioiaronka1@gmail.com>
2025-09-04 14:57:50 -0300

    feat(entities) Added user entity

---
+++ src/core/entities/user.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import Optional
+
+from src.core.entities.user_role import UserRole
+
+
+@dataclass
+class User:
+    id: Optional[int]
+    name: str
+    email: str
+    password: str
+    role: UserRole
+
+    created_at: Optional[datetime] = None
+    updated_at: Optional[datetime] = None
---
+++ src/core/entities/user_role.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class UserRole(Enum):
+    ADMIN = "admin"
+    SELLER = "seller"
+    STOCK_MANAGER = "stock_manager"
